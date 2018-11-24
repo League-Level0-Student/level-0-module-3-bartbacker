1464381e40cf40f7254db7b8fd230d3fdc319cab
bartbacker <league@imac-6.attlocal.net>
2018-11-24 14:27:03 -0800

    banana

--- src/extra/BananaQuiz.java
+++ src/extra/BananaQuiz.java
@@ -10,15 +10,29 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
+		String b = JOptionPane.showInputDialog("Do u like bananz?");
 		//2. if they say no, 
+		if(b.equalsIgnoreCase("no")) {
+			JOptionPane.showMessageDialog(null, "you're crazy!");
+		}
 			//tell them they are crazy 
 			//and end quiz
 		//3. if they say yes
+		else if(b.equalsIgnoreCase("yes")) {
+			String h = JOptionPane.showInputDialog(null, "What's your favorite hob?");
+			JOptionPane.showMessageDialog(null, h+" is much better with bananas!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You are bananas!");
+			
+		}
+	
+		
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
-
-		//4. OPTIONAL: if they say something other than “yes�? or “no�?
-		//	show a pop up that says “You are bananas!�?
+		
+		//4. OPTIONAL: if they say something other than “yes�? or “no�?
+		//	show a pop up that says “You are bananas!�?
 	
 	}
 
