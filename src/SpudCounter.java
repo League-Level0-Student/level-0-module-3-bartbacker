792e5e9d0ec42058792403c648b3be474f7d1a12
bartbacker <league@wts13.attlocal.net>
2018-12-08 14:27:23 -0800

    check point

---
+++ src/SpudCounter.java
@@ -0,0 +1,16 @@
+
+public class SpudCounter {
+	public static void main(String[] args) {
+		for(int i = 1;i < 17;i++){   
+			
+		if(i%8==0) {
+			System.out.println("more");}
+			else if(i%2==0) {
+				System.out.println(i+" potato");}
+			else {
+				System.out.println(i+" potato");}
+			}
+		}
+		
+	}
+
